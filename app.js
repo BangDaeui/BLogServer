35477460c05d8c4f33d674420bdec619056f9468
BangDaeui <33365849+BangDaeui@users.noreply.github.com>
2019-10-24 17:50:00 +0900

    Update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// express
+// express 웹서버 돌리려고
 const express = require('express');
 const app = express();
 
-// path
+// path 폴더 경로
 const path = require('path');
 
-// handlebars
+// handlebars 웹에 데이터 보여줄려고
 const exphbs = require('express-handlebars');
 
-// mysql
+// mysql SQL
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
@@ -26,7 +26,7 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-// Static folder
+// Static folder 폴더 사용
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // [Get] Users
@@ -34,24 +34,17 @@ app.get('/Users.html', async (req, res) => {
     res.render('Users', {});
 })
 
-// [Get] UserLogs
+// [Get] UserLogs http://localhost:3000/UserLogs.html
 app.get('/UserLogs.html', async (req, res) => {
+    
+    // View폴더 안에있는 UserLogs.handlebars 파일 보이게 해줌
     res.render('UserLogs', {});
+    
 })
 
 // [Get] Logs
 app.get('/Logs.html', async (req, res) => {
     res.render('Logs', {});
-})
-
-// [Get] tables
-app.get('/tables', (req, res) => {
-    var sql1 = 'select * from ExampleTable';
-    conn.query(sql1, function (err, result, fields) {
-        res.render('tables', {
-            result:result
-        });
-    }); 
 })
 
 // [Get] table
