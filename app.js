f9ce61700b91939f797e7148edccfc6b8da01400
rydud <54888464+rydud@users.noreply.github.com>
2019-10-29 19:39:49 +0900

    young_1029
    User Logs & Application Log 데이터 불러오기

--- app.js
+++ app.js
@@ -41,22 +41,45 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // [Get] Users
 // 데이터베이스에 있는 사용자 데이터 웹에 보여주기
 app.get('/Users', async (req, res) => {
-    res.render('Users', {});
+    var sql1 = 'select * from User';
+
+    conn.query(sql1, function (err, result, fields) {
+        res.render('Users', {
+            result: result
+        });
+    });
 })
 
 // [Get] UserLogs http://localhost:3000/UserLogs
 // 데이터베이스에 있는 사용자 별 데이터 웹에 보여주기
-app.get('/UserLogs', async (req, res) => {
-    
+app.get('/UserLogs/:id', async (req, res) => {
+    var id = req.params.id;
+    console.log(id);
+    var sql1 = 'select * from User where User_No = ?';
+    var sql2 = 'select * from ApplicationLog where App_User =?';
     // View폴더 안에있는 UserLogs.handlebars 파일 보이게 해줌
-    res.render('UserLogs', {});
-    
+    conn.query(sql1, [id], function (err, result1, fields) {
+        conn.query(sql2, [id], function (err, result2, fields) {
+            res.render('UserLogs', {
+                result1: result1,
+                result2: result2
+            });
+        });
+    });
+
 })
 
 // [Get] Logs
 // 데이터베이스에 있는 로그 데이터 웹에 보여주기
 app.get('/Logs', async (req, res) => {
-    res.render('Logs', {});
+    var sql1 = 'select * from User, ApplicationLog where User_No = App_User;';
+    conn.query(sql1, function (err, result, fields) {
+            res.render('Logs', {
+                result: result
+                
+            });
+        });
+
 })
 
 // [Get] table
@@ -65,16 +88,16 @@ app.get('/table', (req, res) => {
     var sql1 = 'select * from ExampleTable';
     conn.query(sql1, function (err, result, fields) {
         res.render('table', {
-            result:result
+            result: result
         });
-    }); 
+    });
 })
 
 // [Get] Dashboard
 app.get('/', async (req, res) => {
     res.render('Users', {});
 })
- 
+
 app.listen(3000, () => console.log('Server Start'));
 
 // #################################################################### Socket
@@ -83,15 +106,15 @@ var server = net.createServer(function (socket) {
     socket.on('data', function (data) {
         console.log(data);
     });
-        
+
     socket.on('close', function () {
         console.log('Client disconnted');
     });
-        
+
     server.on('error', function (err) {
         console.log('err' + err);
     });
-}); 
+});
 
 
 server.listen(9000, function () {
